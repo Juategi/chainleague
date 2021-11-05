c33d6f15e0e64ebf15fd0eeb5f0c2415f15d8f3b
Juan Tena Gimeno <61567119+Juategi@users.noreply.github.com>
2021-11-05 19:54:38 +0100

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,35 @@
 <template>
-  <div >
+  <div class="page">
+    <div >
     <div class="top">
-      <div style="width: 30%; float: left; overflow: hidden; position:relative">
-        <div style="width: 20%; float: left; overflow: hidden; position:relative">
-          <img src="./assets/icon.svg" class="logo"> 
-        </div>
-        <div style="margin-top: 30px; width: 80%; float: left; overflow: hidden; position:relative">
-          <b class="title" >Chain League</b>
-        </div>
+    <div style="margin-top: 10px; height: 70px; width: 100%; float: left; overflow:hidden; position:relative">  
+      <img src="./assets/icon.svg" class="logo">
+      <b class="title" >Chain League</b>
+      <div style="margin-top:20px">       
+        <b class="bar">Whitepaper</b>
+        <b class="bar" @click="goto('roadmap')">Roadmap</b>
+        <b class="bar"  @click="goto('team')">Team</b> 
+         <button style="margin-left:25px; bottom:15px">Sign up</button>
       </div>
-    <div style="margin-top: 50px; height: 50px; width: 70%; float: right; overflow: hidden; position:relative">
-      <b class="bar">Whitepaper</b>
-      <b class="bar">Roadmap</b>
-      <b class="bar">Team</b>
-      <button style="margin-top:10px; margin-left:10px">Sign up</button>
     </div>
     
   </div>
 
   <div style="height:350px; width: 1200px;">
     <div style="margin-left: 20px; width: 41%; float: left; overflow: hidden; margin-top:50px">
-          <p >The first play-to-earn token based on League of Legends.</p>
+          <p >The first play-to-earn cryptocurrency based on League of Legends.</p>
           <p >Win tokens just by winning normal and ranked games in League of Legends.</p>
-          <p style="font-weight: bold;">Enter the Rift, enter the Blockchain.</p>      
+          <p style="font-weight: bold; ">Enter the Rift, enter the Blockchain.</p>  
+          <p style="">Powered by</p>
+          <div >
+            <div style="float: left; overflow:hidden; margin-left: 115px;">
+              <p style="font-weight: bold; color:#F3BA2F; ">BINANCE <span style="font-weight: normal; ">SMART CHAIN</span> </p> 
+            </div>
+            <div style="float: left; margin-top: 18px; margin-left: 13px">
+              <img src="./assets/bsc.svg" class="bsc"> 
+            </div>
+          </div>
+    
     </div>   
 
     <div style="margin-right: 20px; width: 41%; float: right; overflow: hidden; margin-top:50px">
@@ -49,7 +56,7 @@
     <div style="margin-top: 40px; float: left; overflow: hidden; ">
             <vue3-autocounter ref='counter' :startAmount='0' :endAmount='200000' :duration='3' prefix='+' suffix=' holders' separator=',' decimalSeparator='.' :decimals='0' :autoinit='false' style="font: 40px 'Rubik' ; font-weight: bold; color:#ffffff"  @finished='alert(`Counting finished!`)' />
           <p style="margin-top: 40px; font-weight: bold; font-size: 25px">Total ICO Launch invested</p>
-          <p style="margin-top: 40px; font-weight: bold;">Phase 10</p>   
+          <p style="margin-top: 40px; font-weight: bold;">Phase 10 - Diamond</p>   
           <div style="margin-top: 40px; margin-left: 30px; width: 100%; float: center; overflow: hidden;">
             <b style="font-weight: bold; font-size: 30px; ">100.350$</b>  
             <b style="font-weight: bold; font-size: 30px; color: #ffffff;" >/</b>  
@@ -64,14 +71,20 @@
             <button class="white">Buy tokens</button>
           </div>
     </div>  
+    <div  style="margin-top: 40px; float: right; overflow: hidden; ">
+      <img src="./assets/dia.png" class="rank">
+      <div class="wrap"></div>
+    </div>
   </div>
 
   <div class="mid2">
         <p style="font-weight: bold; font-size: 30px; ">Join our community</p>  
         <img src="./assets/dc.png" class="media" >
         <img src="./assets/ig.png" class="media" >
+        <img src="./assets/tg.png" class="media" >
         <img src="./assets/fb.png" class="media" >
         <img src="./assets/tw.png" class="media" >
+        
   </div>
 
   <div style="height:950px; width: 1200px;">
@@ -86,7 +99,7 @@
 
   <div class="line">  </div>
 
-  <div class="mid3">
+  <div class="mid3" ref="roadmap">
       
   </div>
 
@@ -95,14 +108,14 @@
   </div>
 
   <div class="bot">
-      <p style="padding-top: 250px; font-weight: bold; font-size: 35px">What is League of Legends?</p>
-      <p style="font-size: 25px; cursor: pointer; color: #2588B2" href="https://www.leagueoflegends.com/">Check the website</p>     
+      <p style="padding-top: 200px; font-weight: bold; font-size: 35px">What is League of Legends?</p>
+      <p style="font-size: 25px; cursor: pointer; color: #ffffff" href="https://www.leagueoflegends.com/">Check the website</p>     
       <p style="margin-top: 140px; font-weight: bold; font-size: 35px">+100 million players around the world</p>  
       <p style="margin-top: 140px; font-weight: bold; font-size: 20px">Disclaimer:</p>  
       <p style="font-size: 20px">We are not owned or associated with Riot Games or any of its brands  </p>        
   </div>
 
-  <div style="height:600px; width: 1200px;">
+  <div style="height:600px; width: 1200px;" ref="team">
     <div style="width: 100%; float: center; overflow: hidden; ">     
         <p style="padding-top: 10px; font-weight: bold; font-size: 35px">Team</p>   
     </div>  
@@ -110,7 +123,7 @@
       <div style="margin-left: 100px; width: 40%; float: left; overflow: hidden;">
         <img src="./assets/p1.png" style="width:60%; height:auto; max-width:500px; " >
         <p style="font-weight: bold; font-size: 20px">Juan Tena Gimeno</p>  
-        <p style="font-size: 20px">Full-Stack & Blockchain developer</p>   
+        <p style="font-size: 20px">Full-Stack Developer</p>   
       </div>
       <div style="margin-left: 100px; width: 40%; float: right; overflow: hidden;">
         <img src="./assets/p2.png" style="width:60%; height:auto; max-width:500px;" >
@@ -122,10 +135,10 @@
 
   <div class="fodder">
     <div >
-      <div style="width: 25%;  float:left; overflow: hidden;">          
-        
+      <div style="width: 10%;  float:left; overflow: hidden;">          
+        <img src="./assets/iconw.svg" class="logoB">
       </div>  
-      <div style="width: 25%; float: left; overflow: hidden; ">          
+      <div style="width: 20%; float: left; overflow: hidden; ">          
         <p style="font-weight: bold; font-size: 20px">Contact</p> 
          <p style="font-size: 20px; margin-top:40px;">support@chainleague.com</p>       
       </div>  
@@ -134,23 +147,34 @@
          <p style="font-size: 20px; margin-top:40px; cursor: pointer; ">Terms of Use</p>   
          <p style="font-size: 15px; margin-top:60px; ">© 2022 ChainLeague All Rights Reserved.</p>
       </div>  
-      <div style="width: 25%; float: left; overflow: hidden; ">          
+      <div style="width: 20%; float: left; overflow: hidden; ">          
         <p style="font-weight: bold; font-size: 20px">Social</p> 
         <div>
           <img src="./assets/dc.png" class="mediaS" >
           <img src="./assets/ig.png" class="mediaS" >
+          <img src="./assets/tg.png" class="mediaS" >
           <img src="./assets/fb.png" class="mediaS" >
           <img src="./assets/tw.png" class="mediaS" >
-        </div>   
+        </div> 
+        <div >
+            <div style="float: left; overflow:hidden; margin-left: 30px; ">
+              <p style="font-weight: bold; color:#F3BA2F; ">BINANCE <span style="font-weight: normal; ">SMART CHAIN</span> </p> 
+            </div>
+            <div style="float: left; margin-top: 18px; margin-left: 13px">
+              <img src="./assets/bsc.svg" class="bsc"> 
+            </div>
+          </div>  
       </div>  
     </div>
 
   </div>
 </div>
+  </div>
 </template>
 
 <script>
 import Vue3autocounter from 'vue3-autocounter';
+import './styles.css'
 
 scroll = false
 
@@ -178,6 +202,11 @@ export default ({
       }
       else
         window.removeEventListener('scroll', this.handleScroll);
+    },
+    goto(refName) {
+      var element = this.$refs[refName];
+      var top = element.offsetTop;
+      window.scrollTo(0, top);
     }
 },
   
@@ -185,360 +214,6 @@ export default ({
 })
 </script>
 
-<style>
-#app {
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    background: #050617;  
-}
-
-
-div{
-  margin:0 auto;
-}
-
-b{
-  font: 20px 'Rubik' ;
-  font-weight: bold;
-  color: #2588B2;
-  padding-right: 20px;
-}
-
-p{
-  font: 20px 'Rubik' ;
-  color: #ffffff;
-}
-
-b.bar{
-  cursor: pointer; 
-  padding-right: 100px;
-}
-
-b.bar:hover{
-  color: #15a0db;
-}
-
-
-button{
-  font: 15px 'Rubik' ;
-  font-weight: bold;
-  color: #ffffff;
-  background: #2588B2;
-  border-top-left-radius: 80px;
-  border-top-right-radius: 80px;
-  border-bottom-left-radius: 80px;
-  border-bottom-right-radius: 80px;
-  overflow: hidden;
-  border: 2px solid #ffffff;
-  position: absolute;
-  width: 150px;
-  height: 40px;
-  cursor: pointer;
-}
-
-button:hover {
-  transform: scale(1.1) 
-}
-
-button.white{
-  font: 20px 'Rubik' ;
-  font-weight: bold;
-  color: #2588B2;
-  background: #ffffff;
-  border-top-left-radius: 80px;
-  border-top-right-radius: 80px;
-  border-bottom-left-radius: 80px;
-  border-bottom-right-radius: 80px;
-  overflow: hidden;
-  border: 2px solid #2588B2;
-  width: 180px;
-  height: 60px;
-}
-
-button.white:hover {
-  transform: scale(1.2)
-}
-
-button.big{
-  font: 20px 'Rubik' ;
-  font-weight: bold;
-  color: #ffffff;
-  background: #2588B2;
-  border-top-left-radius: 80px;
-  border-top-right-radius: 80px;
-  border-bottom-left-radius: 80px;
-  border-bottom-right-radius: 80px;
-  overflow: hidden;
-  border: 2px solid #ffffff;
-  width: 220px;
-  height: 60px;
-}
-
-button.big:hover {
-  transform: scale(1.2) 
-}
-
-.title{
-  font: 35px 'Rubik' ;
-  font-weight: bold;
-  color: #2588B2;
-  cursor: pointer; 
-}
-
-.logo{
-  width:60px; 
-  height:60px; 
-  max-width:100px; 
-  padding-right: 20px;
-  padding-top: 20px;
-  cursor: pointer; 
-}
-
-img.media{
-   width:80px; 
-   height:auto; 
-   max-width:80px; 
-   margin-right: 30px; 
-   margin-top: 15px; 
-   position:relative; 
-   cursor: pointer; 
-}
-
-img.mediaS{
-   width:50px; 
-   height:auto; 
-   max-width:50px; 
-   margin-right: 10px; 
-   margin-top: 5px; 
-   position:relative; 
-   cursor: pointer; 
-}
-
-img.media:hover{
-  transform: scale(1.2)
-}
-
-img.mediaS:hover{
-  transform: scale(1.2)
-}
-
-.top{
-  background: url("./assets/B1.png");
-  background-size: 100%;
-  height: 500px;
-  min-height: 500px;
-  min-width: 1200px;
-  max-width: 2000px;
-  background-repeat: no-repeat;
-  margin:0 auto;
-}
-
-.mid{
-  background: url("./assets/B2.png");
-  background-size: 100%;
-  height: 400px;
-  min-height: 500px;
-  min-width: 1200px;
-  max-width: 2000px;
-  background-repeat: no-repeat;
-  margin:0 auto;
-}
-
-.mid2{
-  background: url("./assets/B3.png");
-  background-size: 100%;
-  height: 200px;
-  min-height: 200px;
-  min-width: 1200px;
-  max-width: 2000px;
-  margin:0 auto;
-  background-repeat: no-repeat;
-}
-
-.mid3{
-  background: url("./assets/B5.png");
-  background-size: 100%;
-  height: 600px;
-  width: 1100px;
-  min-height: 600px;
-  min-width: 1100px;
-  max-width: 1100px;
-  margin:0 auto;
-  background-repeat: no-repeat;
-}
-
-.how{
-  background: url("./assets/B7.png");
-  background-size: 100%;
-  height: 600px;
-  min-height: 600px;
-  min-width: 900px;
-  max-width: 900px;
-  margin:0 auto;
-  margin-top: 20px; 
-  background-repeat: no-repeat;
-}
-
-.bot{
-  background: url("./assets/B4.png");
-  background-size: 100%;
-  height: 800px;
-  min-height: 800px;
-  min-width: 1300px;
-  max-width: 2000px;
-  margin:0 auto;
-  background-repeat: no-repeat;
-}
-
-.fodder{
-  background: url("./assets/B6.png");
-  background-size: 100%;
-  height: 200px;
-  min-height: 200px;
-  min-width: 1700px;
-  max-width: 2000px;
-  margin:0 auto;
-  background-repeat: no-repeat;
-}
-
-.line{
-  background: #ffffff;
-  height: 2px;
-  width: 3000px;
-  margin-bottom: 50px; 
-  margin-top: 20px;
-}
-
-
-.rectangleWhite {
-  margin:auto;
-  height: 40px;
-  width: 800px;
-  background-color: #ffffff;
-  border-top-left-radius: 80px;
-  border-top-right-radius: 80px;
-  border-bottom-left-radius: 80px;
-  border-bottom-right-radius: 80px;
-}
-.rectangleBlue {
-  margin:auto;
-  height: 40px;
-  background-color: #2588B2;
-  transition: width 4s linear;
-  transition-delay: 0s;
-  position: absolute;
-  top: 0;
-  left: 0;
-  margin-left: 195px;
-  border-top-left-radius: 80px;
-  border-top-right-radius: 80px;
-  border-bottom-left-radius: 80px;
-  border-bottom-right-radius: 80px;
-}
-
-
-.page{
-  height: 6200px;
-}
-
-@media screen and (max-width: 1100px){
-
-  body {
-   transform: scale(0.7);
-   transform-origin: 0 0;
-   background: #050617;   
-  }
-
-}
-
-
-@media screen and (max-width: 700px){
-
-  body {
-   transform: scale(0.4);
-   transform-origin: 0 0;
-   background: #050617;   
-  }
-
-  .page{
-    height: 200px;
-  }
-}
-
-
-
-/*@media screen and (max-width: 1400px){
-
-  b{
-  font: 20px 'Rubik' ;
-  font-weight: bold;
-  color: #2588B2;
-  margin-right: 60px;
-}
-
-.title{
-  font: 40px 'Rubik' ;
-  font-weight: bold;
-  color: #2588B2;
-  margin-right: 120px;
-}
-
-}
-
-@media screen and (max-width: 960px){
-
-  b{
-  font: 15px 'Rubik' ;
-  font-weight: bold;
-  color: #2588B2;
-  margin-right: 45px;
-}
-
-.title{
-  font: 30px 'Rubik' ;
-  font-weight: bold;
-  color: #2588B2;
-  margin-right: 90px;
-}
-
-}
-
- @media screen and (max-width: 700px){
-
-  b{
-  font: 10px 'Rubik' ;
-  font-weight: bold;
-  color: #2588B2;
-  margin-right: 30px;
-}
-
-.title{
-  font: 20px 'Rubik' ;
-  font-weight: bold;
-  color: #2588B2;0
-}
-
-}
-
-@media screen and (max-width: 560px){
-
-  b{
-  font: 5px 'Rubik' ;
-  font-weight: bold;
-  color: #2588B2;
-  margin-right: 15px;
-}
-
-.title{
-  font: 10px 'Rubik' ;
-  font-weight: bold;
-  color: #2588B2;
-  margin-right: 30px;
-}
-  
-
-} */
-
-
+<style scoped>
+  @import './styles.css';
 </style>
--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import Vue3Autocounter from 'vue3-autocounter';
+import './styles.css';
 
 createApp(App).use(router).component('vue3-autocounter', Vue3Autocounter).mount('#app')
