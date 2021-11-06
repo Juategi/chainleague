ac5565af74424f945a4cbba4b0b8e761fc223259
Juan Tena Gimeno <61567119+Juategi@users.noreply.github.com>
2021-11-06 03:15:32 +0100

    commit

--- src/App.vue
+++ src/App.vue
@@ -15,12 +15,14 @@
     
   </div>
 
-  <div style="height:350px; width: 1200px;">
+  <div style="height:360px; width: 1200px;">
     <div style="margin-left: 20px; width: 41%; float: left; overflow: hidden; margin-top:50px">
           <p >The first play-to-earn cryptocurrency based on League of Legends.</p>
           <p >Win tokens just by winning normal and ranked games in League of Legends.</p>
           <p style="font-weight: bold; ">Enter the Rift, enter the Blockchain.</p>  
-          <p style="">Powered by</p>
+          <div style="margin-top: 18px; ">
+             <p >Powered by</p>
+          </div>
           <div >
             <div style="float: left; overflow:hidden; margin-left: 115px;">
               <p style="font-weight: bold; color:#F3BA2F; ">BINANCE <span style="font-weight: normal; ">SMART CHAIN</span> </p> 
@@ -38,53 +40,37 @@
           <div style="text-align: center; margin-right:180px">
              <button class="white" style="margin-top:10px; ">Buy tokens</button>
           </div>
+          <div style="padding-top: 120px;">
+            <img src="./assets/uni.png" class="uni" >
+          </div>
+          
     </div>  
   </div>
+
+  
 
   <div class="mid">
-      <div style="padding-right: 150px;">
-        <div style="padding-top: 100px;">
-        <button class="big">Sign up</button>
-      </div>
-      <div style="padding-top: 250px;">
-        <button class="big">Log in</button>
-      </div>
-      </div>
-  </div>
-  
-  <div style="height: 630px; width: 1200px;">
-    <div style="margin-top: 40px; float: left; overflow: hidden; ">
-            <vue3-autocounter ref='counter' :startAmount='0' :endAmount='200000' :duration='3' prefix='+' suffix=' holders' separator=',' decimalSeparator='.' :decimals='0' :autoinit='false' style="font: 40px 'Rubik' ; font-weight: bold; color:#ffffff"  @finished='alert(`Counting finished!`)' />
-          <p style="margin-top: 40px; font-weight: bold; font-size: 25px">Total ICO Launch invested</p>
-          <p style="margin-top: 40px; font-weight: bold;">Phase 10 - Diamond</p>   
-          <div style="margin-top: 40px; margin-left: 30px; width: 100%; float: center; overflow: hidden;">
-            <b style="font-weight: bold; font-size: 30px; ">100.350$</b>  
-            <b style="font-weight: bold; font-size: 30px; color: #ffffff;" >/</b>  
-            <b style="font-weight: bold; font-size: 30px; color: #ffffff; ">200.000$</b>     
-          </div>   
-          <div style="margin-top: 40px;  width: 100%; float: center; overflow: hidden; text-align:center; position:relative">
-            <div class="rectangleWhite"></div>
-            <div class="rectangleBlue" ref="bar"></div>
-          </div>     
-          <p style="margin-top: 40px; font-weight: bold; font-size: 30px">Actual price: 0.14$</p>  
-          <div style="margin-right: 180px; margin-top: 80px; ">
+      <div style="height:360px; width: 1200px;">
+    <div style="width: 40%; float: left; overflow: hidden;">
+    </div>   
+
+    <div style="margin-left: 400px; width: 20%; float: left; overflow: hidden; margin-top:50px">
+      <vue3-autocounter ref='counter' :startAmount='0' :endAmount='200000' :duration='3' prefix='+' suffix=' holders' separator=',' decimalSeparator='.' :decimals='0' :autoinit='false' style="font: 25px 'Rubik' ; font-weight: bold; color:#ffffff"  @finished='alert(`Counting finished!`)' />
+          <p style="margin-top: 40px; font-weight: bold; font-size: 15px">Total ICO Launch invested</p>
+           <b style="margin-top: 10px; font-weight: bold; font-size: 20px; ">100.350$</b>   
+          <p style="margin-top: 40px; font-weight: bold;">Phase 10 - Diamond</p>                                      
+          <p style="margin-top: 40px; font-weight: bold; font-size: 20px">Actual price: 0.14$</p>  
+          <div style="margin-right: 180px; margin-top: 40px; ">
             <button class="white">Buy tokens</button>
           </div>
+    
+    </div> 
+    <div style="margin-right: 20px; width: 20%; float: left; overflow: hidden; margin-top:150px">
+          <img src="./assets/dia.png" class="rank">
     </div>  
-    <div  style="margin-top: 40px; float: right; overflow: hidden; ">
-      <img src="./assets/dia.png" class="rank">
-      <div class="wrap"></div>
-    </div>
+    <div style="width: 20%; float: left; overflow: hidden;">
+    </div>  
   </div>
-
-  <div class="mid2">
-        <p style="font-weight: bold; font-size: 30px; ">Join our community</p>  
-        <img src="./assets/dc.png" class="media" >
-        <img src="./assets/ig.png" class="media" >
-        <img src="./assets/tg.png" class="media" >
-        <img src="./assets/fb.png" class="media" >
-        <img src="./assets/tw.png" class="media" >
-        
   </div>
 
   <div style="height:950px; width: 1200px;">
@@ -106,7 +92,7 @@
   <div style=" height: 50px ">
         <p style="font-size: 20px; cursor: pointer;">Check the detailed Roadmap >></p>   
   </div>
-
+  
   <div class="bot">
       <p style="padding-top: 200px; font-weight: bold; font-size: 35px">What is League of Legends?</p>
       <p style="font-size: 25px; cursor: pointer; color: #ffffff" href="https://www.leagueoflegends.com/">Check the website</p>     
@@ -128,10 +114,23 @@
       <div style="margin-left: 100px; width: 40%; float: right; overflow: hidden;">
         <img src="./assets/p2.png" style="width:60%; height:auto; max-width:500px;" >
         <p style="font-weight: bold; font-size: 20px">Enrique Martinez Durá</p>  
-        <p style="font-size: 20px">Marketing Manager, Community Manager & Business Administrator</p> 
+        <p style="font-size: 20px">Marketing Manager & Community Manager</p> 
       </div>
     </div>
   </div>
+
+
+
+  <div class="mid2">
+        <p style="font-weight: bold; font-size: 30px; ">Join our community</p>  
+        <img src="./assets/dc.png" class="media" >
+        <img src="./assets/ig.png" class="media" >
+        <img src="./assets/tg.png" class="media" >
+        <img src="./assets/fb.png" class="media" >
+        <img src="./assets/tw.png" class="media" >
+        
+  </div>
+
 
   <div class="fodder">
     <div >
@@ -140,31 +139,13 @@
       </div>  
       <div style="width: 20%; float: left; overflow: hidden; ">          
         <p style="font-weight: bold; font-size: 20px">Contact</p> 
-         <p style="font-size: 20px; margin-top:40px;">support@chainleague.com</p>       
-      </div>  
-      <div style="width: 25%; float:left; overflow: hidden; ">          
+         <p style="font-size: 20px; margin-top:40px;">support@chainleague.com</p>    
+          <p style="font-size: 15px; margin-top:60px; ">© 2022 ChainLeague All Rights Reserved.</p>  
+      </div>    
+      <div style="width: 10%; float:left; overflow: hidden; ">          
         <p style="font-weight: bold; font-size: 20px">Legal</p>  
          <p style="font-size: 20px; margin-top:40px; cursor: pointer; ">Terms of Use</p>   
-         <p style="font-size: 15px; margin-top:60px; ">© 2022 ChainLeague All Rights Reserved.</p>
-      </div>  
-      <div style="width: 20%; float: left; overflow: hidden; ">          
-        <p style="font-weight: bold; font-size: 20px">Social</p> 
-        <div>
-          <img src="./assets/dc.png" class="mediaS" >
-          <img src="./assets/ig.png" class="mediaS" >
-          <img src="./assets/tg.png" class="mediaS" >
-          <img src="./assets/fb.png" class="mediaS" >
-          <img src="./assets/tw.png" class="mediaS" >
-        </div> 
-        <div >
-            <div style="float: left; overflow:hidden; margin-left: 30px; ">
-              <p style="font-weight: bold; color:#F3BA2F; ">BINANCE <span style="font-weight: normal; ">SMART CHAIN</span> </p> 
-            </div>
-            <div style="float: left; margin-top: 18px; margin-left: 13px">
-              <img src="./assets/bsc.svg" class="bsc"> 
-            </div>
-          </div>  
-      </div>  
+      </div>     
     </div>
 
   </div>
@@ -179,11 +160,8 @@ import './styles.css'
 scroll = false
 
 export default ({
-  created () {
-    window.addEventListener('scroll', this.handleScroll);
-  },
-  destroyed () {
-    window.removeEventListener('scroll', this.handleScroll);
+  mounted(){
+    this.$refs.counter.start();
   },
   components: {
     'vue3-autocounter': Vue3autocounter
@@ -194,15 +172,6 @@ export default ({
     }
   },
   methods: {
-    handleScroll(event) {
-      if(!scroll){
-        this.$refs.counter.start(); 
-        this.$refs.bar.style.width= "30vw";
-        scroll = true
-      }
-      else
-        window.removeEventListener('scroll', this.handleScroll);
-    },
     goto(refName) {
       var element = this.$refs[refName];
       var top = element.offsetTop;
